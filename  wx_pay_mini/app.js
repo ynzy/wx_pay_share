20c13d2ad09717d05f6f0ba2e14d69ab03b9d0a9
zhangyong <758922096@qq.com>
2020-07-01 11:58:10 +0800

    feat(model:all): 小程序公共机制

---  wx_pay_mini/app.js
+++  wx_pay_mini/app.js
@@ -1,8 +1,18 @@
 /**
  * 小程序的入口
  */
+const api = require('./http/api') 
+const request = require('./http/request')
+let config = request('./env/index.js')
+let env = 'Prod'
+App.version = '1.0.0' //
 App({
   globalData: {
+    api: api, //全局注册api，供全局使用
+    // 全局注册请求方法，
+    // get: request.fetch,
+    // post: () => { },
+
     userInfo: null
   },
   onLaunch: function () {
